b88259d087dcf26b800f8bc139d0a60c4768658a
LISA_COOK <lisabpink@gmail.com>
2020-03-24 14:03:32 -0400

    LDC auth folder/file, imported into server.js

--- api/server.js
+++ api/server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 
 const usersRouter = require("../users/users-router");
+const authRouter = require("../auth/auth-router");
 
 const server = express();
 
 server.use(express.json());
 
 server.use("/api/users", usersRouter);
+server.use("api/auth", authRouter);
 
 server.get("/", (req, res) => {
   res.json({ api: "running" });
---
+++ auth/auth-router.js
@@ -0,0 +1,48 @@
+const bcrypt = require("bcryptjs");
+
+const router = require("express").Router();
+
+const Users = require("../users/users-model");
+
+router.post("/register", (req, res) => {
+  //define what will be the username & password
+  const userInfo = req.body;
+  //hash password
+  const hash = bcrypt.hashSync(userInfo.password, 8);
+  //override password with the hash
+  userInfo.password = hash;
+  //add user to db
+  Users.add(userInfo)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(error => {
+      res.status(500).json({ message: "Error registering user" });
+    });
+});
+
+router.post("/login", (req, res) => {
+  //destructure username and password
+  const { username, password } = req.body;
+
+  //find uesr by username
+  Users.findBy({ username })
+    .then(([user]) => {
+      //if username found & password matches
+      if (user && bcrypt.compareSync(password, user.password)) {
+        //remember the user & save on server
+        req.session.user = {
+          id: user.id,
+          username: user.username
+        };
+        res.status(200).json({ hello: user.username });
+      } else {
+        res.status(401).json({ message: "Invalid credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: "Error finding user" });
+    });
+});
+
+module.exports = router;
